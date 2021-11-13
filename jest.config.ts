c481f8405b142ab13a9edfb60ce0f43c6f12291f
João Pedro <joao.pedroasz00@gmail.com>
2021-11-13 12:01:34 -0300

    chore: add custom paths alias

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,21 @@
 export default {
+  roots: [
+    '<rootDir>/tests'
+  ],
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts'
   ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-
-  roots: [
-    '<rootDir>/src'
-  ],
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@/tests/(.*)': '<rootDir>/tests/$1'
+  },
+  testMatch: ['**/*.spec.ts']
 }
