e091a4a65a472f08cb1ebb82cc225ee185165dd9
João Pedro <joao.pedroasz00@gmail.com>
2021-11-13 14:41:38 -0300

    refactor: turn cpf component on to a class

--- tests/cpf.spec.ts
+++ tests/cpf.spec.ts
@@ -1,38 +1,42 @@
-import { validate } from '@/cpf'
+import { CPF } from '@/cpf'
 
-describe('Validate CPF function', () => {
+const makeSut = (cpf: string): CPF => {
+  return new CPF(cpf)
+}
+
+describe('CPF component', () => {
   test('should return true when CPF is valid', () => {
-    const isValidCPF = validate('289.246.570-20')
+    const isValidCPF = makeSut('289.246.570-20').validate()
 
     expect(isValidCPF).toBeTruthy()
   })
 
   test('should return true when CPF is invalid', () => {
-    const isValidCPF = validate('289.246.570-00')
+    const isValidCPF = makeSut('289.246.570-00').validate()
 
     expect(isValidCPF).toBeFalsy()
   })
 
   test('should return false when CPF is with all numbers equals', () => {
-    const isValidCPF = validate('111.111.111-11')
+    const isValidCPF = makeSut('111.111.111-11').validate()
 
     expect(isValidCPF).toBeFalsy()
   })
 
   test('should return false when CPF smaller than allowed', () => {
-    const isValidCPF = validate('111.111.1')
+    const isValidCPF = makeSut('111.111.1').validate()
 
     expect(isValidCPF).toBeFalsy()
   })
 
   test('should return false when CPF bigger than allowed', () => {
-    const isValidCPF = validate('111.111.111-11111')
+    const isValidCPF = makeSut('111.111.111-11111').validate()
 
     expect(isValidCPF).toBeFalsy()
   })
 
   test('should return false when CPF is empty', () => {
-    const isValidCPF = validate('')
+    const isValidCPF = makeSut('').validate()
 
     expect(isValidCPF).toBeFalsy()
   })
