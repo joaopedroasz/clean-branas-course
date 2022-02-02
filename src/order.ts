f51a9ba4683c9b4d2b6f27ae8f3e5606bfba5c5e
João Pedro <joao.pedroasz00@gmail.com>
2022-02-01 22:08:05 -0300

    feature: ensure Order entity can create a order with many items

--- src/order.ts
+++ src/order.ts
@@ -1,6 +1,7 @@
 import { CPF } from '@/cpf'
-import { Coupon } from './coupon'
-import { OrderItem } from './order-item'
+import { Coupon } from '@/coupon'
+import { Item } from '@/item'
+import { OrderItem } from '@/order-item'
 
 export class Order {
   public id?: string
@@ -15,5 +16,13 @@ export class Order {
     this.id = id
     this.orderItems = []
     this.cpf = new CPF(cpf)
+  }
+
+  public addItem (item: Item, quantity: number): void {
+    if (!item.id) {
+      throw new Error('invalid empty id')
+    }
+
+    this.orderItems.push(new OrderItem(item.id, quantity, item.price))
   }
 }
