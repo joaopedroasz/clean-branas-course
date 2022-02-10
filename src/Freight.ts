d8adaa3b89236e4770b3f133b006a7390b28f5ac
João Pedro <joao.pedroasz00@gmail.com>
2022-02-09 21:24:37 -0300

    feature: ensure Freight entity can calculate freight

--- src/Freight.ts
+++ src/Freight.ts
@@ -3,7 +3,17 @@ import { Item } from './Item'
 export class Freight {
   private readonly item: Item
 
+  private readonly DISTANCE = 1000
+
   constructor (item: Item) {
     this.item = item
+  }
+
+  public calculate (): number {
+    return this.DISTANCE * this.item.calculateVolume() * this.convertDensity()
+  }
+
+  private convertDensity (): number {
+    return this.item.calculateDensity() / 100
   }
 }
