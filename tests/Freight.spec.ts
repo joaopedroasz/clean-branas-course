bef58c69d5ad916fcce6cbc499f2c50a5b28fee5
João Pedro <joao.pedroasz00@gmail.com>
2022-02-09 21:25:07 -0300

    test: ensure Freight entity can calculate freight

--- tests/Freight.spec.ts
+++ tests/Freight.spec.ts
@@ -9,10 +9,16 @@ describe('Freight entity', () => {
   let freight: Freight
 
   beforeEach(() => {
-    freight = makeSut(new Item('Categoria do Item 1', 'Descrição do Item 1', 100, 50, 20, 5, 10))
+    freight = makeSut(new Item('Categoria do Item 1', 'Descrição do Item 1', 100, 200, 100, 50, 40))
   })
 
   test('should be create a freight', () => {
     expect(freight).toBeDefined()
+  })
+
+  test('should calculate freight', () => {
+    const freightPrice = freight.calculate()
+
+    expect(freightPrice).toBe(400)
   })
 })
