3405e2ca6bc1ecfb2b3915b9829b05d45359f605
João Pedro <joao.pedroasz00@gmail.com>
2022-02-10 21:43:17 -0300

    feature: ensure Order entity can calculate freight

--- src/Order.ts
+++ src/Order.ts
@@ -3,12 +3,14 @@ import { Coupon } from '@/Coupon'
 import { Item } from '@/Item'
 import { OrderItem } from '@/OrderItem'
 import { InvalidEmptyID } from './errors'
+import { Freight } from './Freight'
 
 export class Order {
   public id?: string
   public orderItems: OrderItem[]
   public cpf: CPF
   public coupon?: Coupon
+  private freight: number
 
   constructor (
     cpf: string,
@@ -17,16 +19,28 @@ export class Order {
     this.id = id
     this.orderItems = []
     this.cpf = new CPF(cpf)
+    this.freight = 0
   }
 
   private isItemsEmpty (): boolean {
     return !this.orderItems || this.orderItems.length === 0
   }
 
+  public getFreight (): number {
+    return this.freight
+  }
+
   public addItem (item: Item, quantity: number): void {
     if (!item.id) throw new InvalidEmptyID()
 
     this.orderItems.push(new OrderItem(item.id, quantity, item.price))
+    this.calculateFreight(item)
+  }
+
+  private calculateFreight (item: Item): void {
+    const freight = new Freight(item)
+
+    this.freight += freight.calculate()
   }
 
   public addCoupon (coupon: Coupon): void {
