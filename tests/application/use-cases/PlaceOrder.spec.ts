8b3fb98b834eace0d693271d90172553ca0546ef
João Pedro <joao.pedroasz00@gmail.com>
2022-02-25 23:11:25 -0300

    test: ensure PlaceOrder use case can execute the use case

--- tests/application/use-cases/PlaceOrder.spec.ts
+++ tests/application/use-cases/PlaceOrder.spec.ts
@@ -1,4 +1,5 @@
 import { PlaceOrder } from '@/application/use-cases'
+import { PlaceOrderInput } from '@/application/DTO/PlaceOrder'
 
 const makeSut = (): PlaceOrder => {
   return new PlaceOrder()
@@ -9,5 +10,30 @@ describe('Place Order use case', () => {
     const placeOrder = makeSut()
 
     expect(placeOrder).toBeDefined()
+  })
+
+  test('should execute place order use case', async () => {
+    const placeOrderInput: PlaceOrderInput = {
+      CPF: '237.967.084-63',
+      orderItems: [
+        {
+          idItem: 1,
+          quantity: 1
+        },
+        {
+          idItem: 2,
+          quantity: 2
+        },
+        {
+          idItem: 3,
+          quantity: 3
+        }
+      ]
+    }
+    const placeOrder = makeSut()
+    const placeOrderSpy = jest.spyOn(placeOrder, 'execute')
+    await placeOrder.execute(placeOrderInput)
+
+    expect(placeOrderSpy).toHaveBeenCalled()
   })
 })
