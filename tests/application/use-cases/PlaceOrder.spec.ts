1176a06e362fff0fa6809b4c59676b245e3fd5b5
João Pedro <joao.pedroasz00@gmail.com>
2022-02-26 00:02:58 -0300

    test: ensure PlaceOrder use case can calculate order total price

--- tests/application/use-cases/PlaceOrder.spec.ts
+++ tests/application/use-cases/PlaceOrder.spec.ts
@@ -1,39 +1,59 @@
 import { PlaceOrder } from '@/application/use-cases'
 import { PlaceOrderInput } from '@/application/DTO/PlaceOrder'
+import { ItemRepository } from '@/domain/repositories'
+import { ItemRepositoryStub } from '@/tests/stub/repositories'
 
-const makeSut = (): PlaceOrder => {
-  return new PlaceOrder()
+type makeSutTypes = {
+  placeOrder: PlaceOrder
+  itemRepository: ItemRepository
+}
+
+const makeSut = (): makeSutTypes => {
+  const itemRepository = new ItemRepositoryStub()
+  const placeOrder = new PlaceOrder(itemRepository)
+  return {
+    placeOrder,
+    itemRepository
+  }
 }
 
 describe('Place Order use case', () => {
+  const placeOrderInput: PlaceOrderInput = {
+    CPF: '237.967.084-63',
+    orderItems: [
+      {
+        idItem: '1',
+        quantity: 1
+      },
+      {
+        idItem: '2',
+        quantity: 2
+      },
+      {
+        idItem: '3',
+        quantity: 3
+      }
+    ]
+  }
+
   test('should create a place order', () => {
-    const placeOrder = makeSut()
+    const { placeOrder } = makeSut()
 
     expect(placeOrder).toBeDefined()
   })
 
   test('should execute place order use case', async () => {
-    const placeOrderInput: PlaceOrderInput = {
-      CPF: '237.967.084-63',
-      orderItems: [
-        {
-          idItem: 1,
-          quantity: 1
-        },
-        {
-          idItem: 2,
-          quantity: 2
-        },
-        {
-          idItem: 3,
-          quantity: 3
-        }
-      ]
-    }
-    const placeOrder = makeSut()
+    const { placeOrder } = makeSut()
     const placeOrderSpy = jest.spyOn(placeOrder, 'execute')
     await placeOrder.execute(placeOrderInput)
 
     expect(placeOrderSpy).toHaveBeenCalled()
+  })
+
+  test('should return order total price', async () => {
+    const { placeOrder } = makeSut()
+    const { total } = await placeOrder.execute(placeOrderInput)
+
+    expect(total).toBe(300)
   })
 })
