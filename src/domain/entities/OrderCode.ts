e497c193e1e9ac511a37e19065d03e7bdd6b54d0
João Pedro <joao.pedroasz00@gmail.com>
2022-02-28 20:55:31 -0300

    feature: ensure OrderCode entity can generate a code

--- src/domain/entities/OrderCode.ts
+++ src/domain/entities/OrderCode.ts
@@ -1,7 +1,32 @@
 export class OrderCode {
-  private readonly code: string
+  private code: string
+
+  private readonly CONVERT_TO_8_DIGITS_NUMBER_FACTOR = 100_000_000
 
   constructor () {
     this.code = ''
+  }
+
+  public getCode (): string {
+    return this.code
+  }
+
+  public generate (): void {
+    const currentYear = this.getCurrentYear().toString()
+    const hash = this.get8DigitHash().toString()
+
+    this.code = currentYear + hash
+  }
+
+  private getCurrentYear (currentDate = new Date()): number {
+    return currentDate.getFullYear()
+  }
+
+  private get8DigitHash (): number {
+    return Math.floor(this.generate8DigitsNumber())
+  }
+
+  private generate8DigitsNumber (): number {
+    return Math.random() * this.CONVERT_TO_8_DIGITS_NUMBER_FACTOR
   }
 }
