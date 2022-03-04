9c54d5f629f429403d7e1b963bb538980658a808
João Pedro <joao.pedroasz00@gmail.com>
2022-03-03 22:53:21 -0300

    test: ensure SimulateFreight can execute simulate freight use case

--- tests/application/use-cases/SimulateFreight.spec.ts
+++ tests/application/use-cases/SimulateFreight.spec.ts
@@ -1,21 +1,50 @@
+import { ItemRepository } from '@/domain/repositories'
 import { SimulateFreight } from '@/application/use-cases'
+import { SimulateFreightInputProperties } from '@/application/dtos/simulate-freight'
+import { ItemRepositoryStub } from '@/tests/stub/repositories'
 
 type makeSutTypes = {
   simulateFreight: SimulateFreight
+  itemRepository: ItemRepository
 }
 
 const makeSut = (): makeSutTypes => {
-  const simulateFreight = new SimulateFreight()
+  const itemRepository = new ItemRepositoryStub()
+  const simulateFreight = new SimulateFreight(itemRepository)
 
   return {
-    simulateFreight
+    simulateFreight,
+    itemRepository
   }
 }
 
 describe('Simulate Freight use case', () => {
-  const { simulateFreight } = makeSut()
+  const simulateOrderInput: SimulateFreightInputProperties[] = [
+    {
+      itemId: '1',
+      quantity: 1
+    },
+    {
+      itemId: '2',
+      quantity: 2
+    },
+    {
+      itemId: '2',
+      quantity: 3
+    }
+  ]
 
   test('should create a simulate freight', () => {
+    const { simulateFreight } = makeSut()
+
     expect(simulateFreight).toBeDefined()
+  })
+
+  test('should execute simulate freight use case', async () => {
+    const { simulateFreight } = makeSut()
+
+    const { totalValue } = await simulateFreight.execute(simulateOrderInput)
+
+    expect(totalValue).toBe(600)
   })
 })
