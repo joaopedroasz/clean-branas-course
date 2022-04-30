c1caaf3cba5ebcda8e07e4060c35c1a1acf7d7de
João Pedro <joao.pedroasz00@gmail.com>
2022-04-30 16:17:31 -0300

    test: ensure Search Order By Code calls orderRepository wirh correct parameters

--- tests/application/use-cases/unit/SearchOrderByCode.spec.ts
+++ tests/application/use-cases/unit/SearchOrderByCode.spec.ts
@@ -1,19 +1,27 @@
+import { OrderRepository } from '@/domain/repositories'
+
 import { SearchOrderByCodeUseCase } from '@/application/contracts'
 import { SearchOrderByCode } from '@/application/use-cases'
+import { OrderRepositoryStub } from '@/tests/stub/repositories'
 
 type makeSutType = {
+  orderRepository: OrderRepository
   searchOrderByCode: SearchOrderByCodeUseCase
 }
 
 const makeSut = (): makeSutType => {
-  const searchOrderByCode = new SearchOrderByCode()
+  const orderRepository = new OrderRepositoryStub()
+  const searchOrderByCode = new SearchOrderByCode(orderRepository)
 
   return {
-    searchOrderByCode
+    searchOrderByCode,
+    orderRepository
   }
 }
 
 describe('Search Order By Code use case', () => {
+  const validCode = 'valid_code'
+
   test('should create a instance of use case', () => {
     const { searchOrderByCode } = makeSut()
 
@@ -23,13 +31,23 @@ describe('Search Order By Code use case', () => {
 
   test('should search a order by code', async () => {
     const { searchOrderByCode } = makeSut()
-    const code = 'random_code'
 
     const order = await searchOrderByCode.execute({
-      orderCode: code
+      orderCode: validCode
     })
 
     expect(order).toHaveProperty('orderCode')
-    expect(order.orderCode).toBe(code)
+    expect(order.orderCode).toBe(validCode)
+  })
+
+  test('should calls orderRepository with correct parameters', async () => {
+    const { searchOrderByCode, orderRepository } = makeSut()
+    const orderItemRepositorySpy = jest.spyOn(orderRepository, 'getByCode')
+
+    await searchOrderByCode.execute({ orderCode: validCode })
+
+    expect(orderItemRepositorySpy).toHaveBeenCalled()
+    expect(orderItemRepositorySpy).toHaveBeenCalledTimes(1)
+    expect(orderItemRepositorySpy).toHaveBeenCalledWith(validCode)
   })
 })
