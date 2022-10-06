ac65a97ebfdaf70b6d4aa63f2a72d526baaafdc9
João Pedro <joao.pedroasz00@gmail.com>
2022-10-06 19:11:59 -0300

    test: ensure Item entity not create an item with invalid price

---
+++ tests/Item.spec.ts
@@ -0,0 +1,17 @@
+import { Item } from '@/item'
+import { InvalidPriceError } from '@/InvalidPrice'
+
+describe('Item', () => {
+  it('should create an item with description and price', () => {
+    const sut = new Item({ description: 'any_description', price: 1 })
+
+    expect(sut).toBeDefined()
+  })
+
+  it('should not create an item with invalid price', () => {
+    const invalidPrice = -1
+    const sut = (): Item => new Item({ description: 'any_description', price: invalidPrice })
+
+    expect(sut).toThrow(new InvalidPriceError(invalidPrice))
+  })
+})
