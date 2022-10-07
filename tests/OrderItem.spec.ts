6ed35c271d4e55ac482b34113823a9ab1e33e750
João Pedro <joao.pedroasz00@gmail.com>
2022-10-07 15:31:18 -0300

    test: ensure OrderItem can be created with item and quantity

---
+++ tests/OrderItem.spec.ts
@@ -0,0 +1,11 @@
+import { OrderItem } from '@/OrderItem'
+import { Item } from '@/Item'
+
+describe('OrderItem', () => {
+  it('should create an order item with item and quantity', () => {
+    const item = new Item({ description: 'T-Shirt', price: 10 })
+    const sut = new OrderItem({ item, quantity: 2 })
+
+    expect(sut).toBeDefined()
+  })
+})
