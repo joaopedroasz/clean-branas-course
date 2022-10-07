ba45edf864ccd64cf191c9a72e97161dfe3303f4
João Pedro <joao.pedroasz00@gmail.com>
2022-10-07 16:20:32 -0300

    feat: ensure Order can add a coupon

--- src/Order.ts
+++ src/Order.ts
@@ -1,3 +1,4 @@
+import { Coupon } from './Coupon'
 import { CPF } from './CPF'
 import { Item } from './Item'
 import { OrderItem } from './OrderItem'
@@ -14,20 +15,30 @@ export type AddItemProps = {
 export class Order {
   private readonly buyerCPF: CPF
   private readonly orderItems: OrderItem[]
+  private coupon?: Coupon
 
   constructor ({
     buyerCPF
   }: OrderProps) {
     this.buyerCPF = new CPF(buyerCPF)
     this.orderItems = []
+    this.coupon = undefined
   }
 
   public getOrderItems (): OrderItem[] {
     return this.orderItems
   }
 
+  public getCoupon (): Coupon | undefined {
+    return this.coupon
+  }
+
   public addItem ({ item, quantity }: AddItemProps): void {
     this.orderItems.push(new OrderItem({ item, quantity }))
+  }
+
+  public addCoupon (coupon: Coupon): void {
+    this.coupon = coupon
   }
 
   public getTotalPrice (): number {
