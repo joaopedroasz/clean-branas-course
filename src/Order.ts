37622f43f619201e83f18f9ce20851447b814b30
João Pedro <joao.pedroasz00@gmail.com>
2022-10-07 15:54:56 -0300

    feat: ensure Order can be created with 3 items

--- src/Order.ts
+++ src/Order.ts
@@ -1,15 +1,32 @@
 import { CPF } from './CPF'
+import { Item } from './Item'
+import { OrderItem } from './OrderItem'
 
 export type OrderProps = {
   buyerCPF: string
 }
 
+export type AddItemProps = {
+  item: Item
+  quantity: number
+}
+
 export class Order {
   private readonly buyerCPF: CPF
+  private readonly orderItems: OrderItem[]
 
   constructor ({
     buyerCPF
   }: OrderProps) {
     this.buyerCPF = new CPF(buyerCPF)
+    this.orderItems = []
+  }
+
+  public getOrderItems (): OrderItem[] {
+    return this.orderItems
+  }
+
+  public addItem ({ item, quantity }: AddItemProps): void {
+    this.orderItems.push(new OrderItem({ item, quantity }))
   }
 }
