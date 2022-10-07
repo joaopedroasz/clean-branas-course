565ecc1de3f1c97bdde128ad5b7c60153d1612de
João Pedro <joao.pedroasz00@gmail.com>
2022-10-07 16:14:35 -0300

    test: ensure Coupon cannot be created with invalid percentage values

--- tests/Coupon.spec.ts
+++ tests/Coupon.spec.ts
@@ -1,10 +1,47 @@
 import { Coupon } from '@/Coupon'
+import { InvalidPercentageError } from '@/InvalidPercentage'
 
 describe('Coupon', () => {
   it('should create a coupon with valid percentage', () => {
     const coupon = new Coupon({
       code: 'VALE20',
       percentage: 20
+    })
+
+    expect(coupon).toBeDefined()
+  })
+
+  it('should not create a coupon with percentage bigger than 100', () => {
+    const coupon = (): Coupon => new Coupon({
+      code: 'VALE200',
+      percentage: 200
+    })
+
+    expect(coupon).toThrowError(new InvalidPercentageError(200))
+  })
+
+  it('should not create a coupon with percentage less than 0', () => {
+    const coupon = (): Coupon => new Coupon({
+      code: 'VALE-20',
+      percentage: -20
+    })
+
+    expect(coupon).toThrowError(new InvalidPercentageError(-20))
+  })
+
+  it('should not create a coupon with percentage equal to 0', () => {
+    const coupon = (): Coupon => new Coupon({
+      code: 'VALE0',
+      percentage: 0
+    })
+
+    expect(coupon).toThrowError(new InvalidPercentageError(0))
+  })
+
+  it('should create a coupon with percentage equal to 100', () => {
+    const coupon = new Coupon({
+      code: 'VALE100',
+      percentage: 100
     })
 
     expect(coupon).toBeDefined()
