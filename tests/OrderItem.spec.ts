e4a0f86a746196763afd9b00487617574e9a252d
João Pedro <joao.pedroasz00@gmail.com>
2022-10-09 21:46:17 -0300

    refactor: change test pattern on OrderItem entity

--- tests/OrderItem.spec.ts
+++ tests/OrderItem.spec.ts
@@ -1,17 +1,26 @@
-import { OrderItem } from '@/OrderItem'
+import { OrderItem, OrderItemProps } from '@/OrderItem'
 import { Item } from '@/Item'
 
-describe('OrderItem', () => {
-  it('should create an order item with item and quantity', () => {
-    const item = new Item({ description: 'T-Shirt', price: 10 })
-    const sut = new OrderItem({ item, quantity: 2 })
+const makeSut = (props: OrderItemProps): OrderItem => new OrderItem(props)
 
+describe('OrderItem', () => {
+  let sut: OrderItem
+  let item: Item
+
+  beforeEach(() => {
+    item = new Item({
+      description: 'any_description',
+      price: 10
+    })
+    sut = makeSut({ item, quantity: 1 })
+  })
+
+  it('should create an order item with item and quantity', () => {
     expect(sut).toBeDefined()
   })
 
   it('should calculate price of order item', () => {
-    const item = new Item({ description: 'T-Shirt', price: 10 })
-    const sut = new OrderItem({ item, quantity: 2 })
+    const sut = makeSut({ item, quantity: 2 })
 
     expect(sut.calculatePrice()).toBe(20)
   })
