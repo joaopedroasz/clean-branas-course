b4852b86070b587b3eb033243ceb3f6c5bcd9197
João Pedro <joao.pedroasz00@gmail.com>
2022-10-09 21:35:41 -0300

    refactor: change test pattern on Item entity

--- tests/Item.spec.ts
+++ tests/Item.spec.ts
@@ -1,30 +1,38 @@
-import { Item } from '@/Item'
+import { Item, ItemProps } from '@/Item'
 import { InvalidPriceError } from '@/InvalidPrice'
 
-describe('Item', () => {
-  it('should create an item with description and price', () => {
-    const sut = new Item({ description: 'any_description', price: 1 })
+const makeSut = (props?: ItemProps): Item => new Item({
+  description: 'any_description',
+  price: 10,
+  ...props
+})
 
+describe('Item', () => {
+  let sut: Item
+
+  beforeEach(() => {
+    sut = makeSut()
+  })
+
+  it('should create an item with description and price', () => {
     expect(sut).toBeDefined()
   })
 
   it('should not create an item with invalid price', () => {
     const invalidPrice = -1
-    const sut = (): Item => new Item({ description: 'any_description', price: invalidPrice })
+    const sut = (): Item => makeSut({ description: 'any_description', price: invalidPrice })
 
     expect(sut).toThrow(new InvalidPriceError(invalidPrice))
   })
 
   it('should not create an item with price equals to zero', () => {
     const invalidPrice = 0
-    const sut = (): Item => new Item({ description: 'any_description', price: invalidPrice })
+    const sut = (): Item => makeSut({ description: 'any_description', price: invalidPrice })
 
     expect(sut).toThrow(new InvalidPriceError(invalidPrice))
   })
 
   it('should return price', () => {
-    const sut = new Item({ description: 'any_description', price: 1 })
-
-    expect(sut.getPrice()).toBe(1)
+    expect(sut.getPrice()).toBe(10)
   })
 })
