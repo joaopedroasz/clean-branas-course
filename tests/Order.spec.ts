466f209d643efee6b9abb924472d062ed898a820
João Pedro <joao.pedroasz00@gmail.com>
2022-10-09 21:39:00 -0300

    refactor: change test pattern on Order entity

--- tests/Order.spec.ts
+++ tests/Order.spec.ts
@@ -1,26 +1,32 @@
-import { Order } from '@/Order'
+import { Order, OrderProps } from '@/Order'
 import { InvalidCpfError } from '@/InvalidCPF'
 import { Item } from '@/Item'
 import { Coupon } from '@/Coupon'
 
+const makeSut = (props?: OrderProps): Order => new Order({
+  buyerCPF: '607.109.010-54',
+  ...props
+})
+
 describe('Order', () => {
+  let sut: Order
+
+  beforeEach(() => {
+    sut = makeSut()
+  })
+
   it('should not create an order with invalid CPF', () => {
     const invalidCPF = '705.738.222-72'
-    const sut = (): Order => new Order({ buyerCPF: invalidCPF })
+    const sut = (): Order => makeSut({ buyerCPF: invalidCPF })
 
     expect(sut).toThrow(new InvalidCpfError(invalidCPF))
   })
 
   it('should create an order with valid CPF', () => {
-    const validCPF = '705.738.222-71'
-    const sut = new Order({ buyerCPF: validCPF })
-
     expect(sut).toBeDefined()
   })
 
   it('should add OrderItems into Order', () => {
-    const validCPF = '705.738.222-71'
-    const sut = new Order({ buyerCPF: validCPF })
     const item = new Item({ description: 'description', price: 10 })
 
     sut.addItem({ item, quantity: 1 })
@@ -29,8 +35,6 @@ describe('Order', () => {
   })
 
   it('should create an order with three items', async () => {
-    const validCPF = '705.738.222-71'
-    const sut = new Order({ buyerCPF: validCPF })
     const item1 = new Item({ description: 'item 1', price: 10 })
     const item3 = new Item({ description: 'item 2', price: 10 })
     const item2 = new Item({ description: 'item 3', price: 10 })
@@ -43,8 +47,6 @@ describe('Order', () => {
   })
 
   it('should calculate total price', () => {
-    const validCPF = '705.738.222-71'
-    const sut = new Order({ buyerCPF: validCPF })
     const item1 = new Item({ description: 'item 1', price: 10 })
     const item2 = new Item({ description: 'item 2', price: 20 })
     const item3 = new Item({ description: 'item 3', price: 30 })
@@ -57,12 +59,6 @@ describe('Order', () => {
   })
 
   it('should add coupon into order', () => {
-    const validCPF = '705.738.222-71'
-    const sut = new Order({ buyerCPF: validCPF })
-    const item1 = new Item({ description: 'item 1', price: 10 })
-
-    sut.addItem({ item: item1, quantity: 1 })
-
     const coupon = new Coupon({ code: 'VALE20', percentage: 20 })
     sut.addCoupon(coupon)
 
@@ -70,7 +66,6 @@ describe('Order', () => {
   })
 
   it('should calculate total price with coupon discount', () => {
-    const sut = new Order({ buyerCPF: '705.738.222-71' })
     const item1 = new Item({ description: 'item 1', price: 10 })
     const item2 = new Item({ description: 'item 2', price: 20 })
     const item3 = new Item({ description: 'item 3', price: 30 })
