f9ddbf9d4ea558b5d5b75e54adbc17c88ef583e5
João Pedro <joao.pedroasz00@gmail.com>
2022-10-12 17:20:47 -0300

    feat: ensure Dimension not creates with negative values

---
+++ src/Dimensions.ts
@@ -0,0 +1,27 @@
+import { InvalidDimensionError } from '@/InvalidDimension'
+
+export type DimensionsProps = {
+  heightInCm: number
+  widthInCm: number
+  depthInCm: number
+}
+
+export class Dimensions {
+  private readonly heightInCm: number
+  private readonly widthInCm: number
+  private readonly depthInCm: number
+
+  constructor ({ heightInCm, widthInCm, depthInCm }: DimensionsProps) {
+    this.heightInCm = heightInCm
+    this.widthInCm = widthInCm
+    this.depthInCm = depthInCm
+
+    if (!this.isValidDimension(heightInCm)) throw new InvalidDimensionError({ propertyName: 'heightInCm', value: heightInCm })
+    if (!this.isValidDimension(widthInCm)) throw new InvalidDimensionError({ propertyName: 'widthInCm', value: widthInCm })
+    if (!this.isValidDimension(depthInCm)) throw new InvalidDimensionError({ propertyName: 'depthInCm', value: depthInCm })
+  }
+
+  private isValidDimension (value: number): boolean {
+    return value > 0
+  }
+}
