673f608f8ca29fa7c7078dc94717b10bbb9567db
João Pedro <joao.pedroasz00@gmail.com>
2022-10-14 17:06:11 -0300

    test: ensure PlaceOrder returns total price with coupon discount

--- tests/PlaceOrder.spec.ts
+++ tests/PlaceOrder.spec.ts
@@ -1,15 +1,64 @@
 import { PlaceOrder } from '@/PlaceOrder'
 import { PlaceOrderInputDTO } from '@/PlaceOrderInput'
 import { PlaceOrderUseCase } from '@/PlaceOrderUseCase'
+import { GetItemByIdRepository } from '@/GetItemByIdRepository'
+import { Item, ItemProps } from '@/Item'
+import { Coupon, CouponProps } from '@/Coupon'
+import { GetCouponByCodeRepository } from '@/GetCouponByCodeRepository'
+
+const makeCoupon = (props?: Partial<CouponProps>): Coupon => new Coupon({
+  code: 'any_code',
+  percentage: 10,
+  dueDate: new Date('2021-12-31'),
+  today: new Date('2021-01-01')
+})
+
+const makeItem = (props?: Partial<ItemProps>): Item => new Item({
+  id: 'any_id',
+  description: 'any_description',
+  price: 10,
+  depthInCm: 10,
+  heightInCm: 10,
+  weightInKg: 10,
+  widthInCm: 10,
+  ...props
+})
+
+const makeGetItemByIdRepository = (): GetItemByIdRepository => {
+  class GetItemByIdRepositoryStub implements GetItemByIdRepository {
+    async getById (id: string): Promise<Item> {
+      return makeItem({ id })
+    }
+  }
+  return new GetItemByIdRepositoryStub()
+}
+
+const makeGetCouponByCodeRepository = (): GetCouponByCodeRepository => {
+  class GetCouponByCodeRepositoryStub implements GetCouponByCodeRepository {
+    async getByCode (code: string): Promise<Coupon> {
+      return makeCoupon({ code })
+    }
+  }
+  return new GetCouponByCodeRepositoryStub()
+}
 
 type SutType = {
   sut: PlaceOrder
+  getItemByIdRepository: GetItemByIdRepository
+  getCouponByCodeRepository: GetCouponByCodeRepository
 }
 
 const makeSut = (): SutType => {
-  const sut = new PlaceOrderUseCase()
+  const getItemByIdRepository = makeGetItemByIdRepository()
+  const getCouponByCodeRepository = makeGetCouponByCodeRepository()
+  const sut = new PlaceOrderUseCase(
+    getItemByIdRepository,
+    getCouponByCodeRepository
+  )
   return {
-    sut
+    sut,
+    getItemByIdRepository,
+    getCouponByCodeRepository
   }
 }
 
@@ -26,7 +75,22 @@ describe('PlaceOrder use case', () => {
 
     const result = await sut.execute(input)
 
-    expect(result.order).toEqual(100)
-    expect(result.total).toBe(100)
+    expect(result.total).toBe(330)
+  })
+
+  it('should return total value with coupon discount if coupon code provided', async () => {
+    const input: PlaceOrderInputDTO = {
+      buyerCPF: '607.109.010-54',
+      orderItems: [
+        { itemId: 'any_id', quantity: 1 },
+        { itemId: 'other_id', quantity: 2 }
+      ],
+      couponCode: 'any_coupon_with_10_percent_discount'
+    }
+    const { sut } = makeSut()
+
+    const result = await sut.execute(input)
+
+    expect(result.total).toBe(297)
   })
 })
