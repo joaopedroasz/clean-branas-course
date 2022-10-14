a1a90c9f111ec837472c5f5420f4c72e24392b5b
João Pedro <joao.pedroasz00@gmail.com>
2022-10-14 17:05:32 -0300

    refactor: return only total price on place order use case

--- src/PlaceOrderOutput.ts
+++ src/PlaceOrderOutput.ts
@@ -1,21 +1,13 @@
 type PlaceOrderOutputProps = {
-  order: number
-  couponDiscount: number
   total: number
 }
 
 export class PlaceOrderOutputDTO {
-  public readonly order: number
-  public readonly couponDiscount: number
   public readonly total: number
 
   constructor ({
-    couponDiscount,
-    order,
     total
   }: PlaceOrderOutputProps) {
-    this.order = order
-    this.couponDiscount = couponDiscount
     this.total = total
   }
 }
