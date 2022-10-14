d9db86d0509327a5a19976f053c4e88c311c2bf2
João Pedro <joao.pedroasz00@gmail.com>
2022-10-14 17:05:58 -0300

    feat: ensure PlaceOrder returns total price with coupon discount

--- src/PlaceOrderUseCase.ts
+++ src/PlaceOrderUseCase.ts
@@ -1,13 +1,35 @@
+import { GetCouponByCodeRepository } from './GetCouponByCodeRepository'
+import { GetItemByIdRepository } from './GetItemByIdRepository'
+import { Order } from './Order'
 import { PlaceOrder } from './PlaceOrder'
 import { PlaceOrderInputDTO } from './PlaceOrderInput'
 import { PlaceOrderOutputDTO } from './PlaceOrderOutput'
 
 export class PlaceOrderUseCase implements PlaceOrder {
+  private readonly getItemByIdRepository: GetItemByIdRepository
+  private readonly getCouponByCodeRepository: GetCouponByCodeRepository
+
+  constructor (
+    getItemByIdRepository: GetItemByIdRepository,
+    getCouponByCodeRepository: GetCouponByCodeRepository
+  ) {
+    this.getItemByIdRepository = getItemByIdRepository
+    this.getCouponByCodeRepository = getCouponByCodeRepository
+  }
+
   public async execute (input: PlaceOrderInputDTO): Promise<PlaceOrderOutputDTO> {
+    const { buyerCPF, orderItems } = input
+    const order = new Order({ buyerCPF })
+    for (const orderItem of orderItems) {
+      const item = await this.getItemByIdRepository.getById(orderItem.itemId)
+      order.addItem({ item, quantity: orderItem.quantity })
+    }
+    if (input.couponCode) {
+      const coupon = await this.getCouponByCodeRepository.getByCode(input.couponCode)
+      order.addCoupon(coupon)
+    }
     return {
-      order: 100,
-      couponDiscount: 0,
-      total: 100
+      total: order.getTotalPrice()
     }
   }
 }
