959dcc5c3996aff07a3f6efb67a1d53d9267f9d0
João Pedro <joao.pedroasz00@gmail.com>
2022-10-14 14:40:16 -0300

    test: ensure OrderCode generates by date and sequence

---
+++ tests/OrderCode.spec.ts
@@ -0,0 +1,11 @@
+import { OrderCode, OrderCodeProps } from '@/OrderCode'
+
+const makeSut = (props?: Partial<OrderCodeProps>): OrderCode => new OrderCode({ date: new Date(), sequence: 1, ...props })
+
+describe('Order Code', () => {
+  it('should generate an code to order by date and sequence', () => {
+    const orderCode = makeSut({ date: new Date('2022-09-14'), sequence: 0 })
+
+    expect(orderCode.getCode()).toBe('202200000001')
+  })
+})
