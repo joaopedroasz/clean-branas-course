dc801c50f9966ec9b858790b89e6e4358d3b6d31
João Pedro <joao.pedroasz00@gmail.com>
2022-10-17 16:04:01 -0300

    refactor: add date in ValidateCoupon input props

--- src/ValidateCouponInputDTO.ts
+++ src/ValidateCouponInputDTO.ts
@@ -1,11 +1,14 @@
 type ValidateCouponInputProps = {
   couponCode: string
+  date?: Date
 }
 
 export class ValidateCouponInputDTO {
   public readonly couponCode: string
+  public readonly date?: Date
 
-  constructor ({ couponCode }: ValidateCouponInputProps) {
+  constructor ({ couponCode, date }: ValidateCouponInputProps) {
     this.couponCode = couponCode
+    this.date = date
   }
 }
