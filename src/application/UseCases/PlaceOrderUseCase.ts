f5873185a2aeedca7a54faf5517cb580cab87211
João Pedro <joao.pedroasz00@gmail.com>
2022-10-18 17:13:02 -0300

    feat: ensure PlaceOrder calls CountOrdersRepository correctly

--- src/application/UseCases/PlaceOrderUseCase.ts
+++ src/application/UseCases/PlaceOrderUseCase.ts
@@ -1,7 +1,7 @@
 import { Order } from '@/domain/entities'
 import { GetItemByIdRepository } from '@/domain/repositories/Item'
 import { GetCouponByCodeRepository } from '@/domain/repositories/Coupon'
-import { SaveOrderRepository } from '@/domain/repositories/Order'
+import { CountOrdersRepository, SaveOrderRepository } from '@/domain/repositories/Order'
 import { PlaceOrder } from '@/application/contracts'
 import { PlaceOrderInputDTO, PlaceOrderOutputDTO } from '@/application/DTOs'
 
@@ -9,19 +9,23 @@ export class PlaceOrderUseCase implements PlaceOrder {
   private readonly getItemByIdRepository: GetItemByIdRepository
   private readonly getCouponByCodeRepository: GetCouponByCodeRepository
   private readonly saveOrderRepository: SaveOrderRepository
+  private readonly countOrdersRepository: CountOrdersRepository
 
   constructor (
     getItemByIdRepository: GetItemByIdRepository,
     getCouponByCodeRepository: GetCouponByCodeRepository,
-    saveOrderRepository: SaveOrderRepository
+    saveOrderRepository: SaveOrderRepository,
+    countOrdersRepository: CountOrdersRepository
   ) {
     this.getItemByIdRepository = getItemByIdRepository
     this.getCouponByCodeRepository = getCouponByCodeRepository
     this.saveOrderRepository = saveOrderRepository
+    this.countOrdersRepository = countOrdersRepository
   }
 
   public async execute (input: PlaceOrderInputDTO): Promise<PlaceOrderOutputDTO> {
     const { buyerCPF, orderItems, couponCode, purchaseDate } = input
+    await this.countOrdersRepository.count()
     const order = new Order({ buyerCPF, purchaseDate, sequence: 1 })
     for (const orderItem of orderItems) {
       const item = await this.getItemByIdRepository.getById(orderItem.itemId)
