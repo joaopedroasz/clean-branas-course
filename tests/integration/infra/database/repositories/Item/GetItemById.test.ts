ea983a194ad4a00d39475a7ca73793e2ef474947
João Pedro <joao.pedroasz00@gmail.com>
2022-10-19 18:52:45 -0300

    test: ensure GetItemByIdRepository search item correctly

---
+++ tests/integration/infra/database/repositories/Item/GetItemById.test.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client'
+
+import { GetItemByIdRepository } from '@/domain/repositories/Item'
+import { ItemNotFoundError } from '@/domain/errors'
+import { GetItemByIdPostgresRepository } from '@/infra/database'
+
+type SutTypes = {
+  sut: GetItemByIdRepository
+  connection: PrismaClient
+}
+
+const makeSut = (): SutTypes => {
+  const connection = new PrismaClient()
+  const sut = new GetItemByIdPostgresRepository(connection)
+  return {
+    sut,
+    connection
+  }
+}
+
+describe('GetItemByIdPostgresRepository', () => {
+  beforeAll(async () => {
+    const { connection } = makeSut()
+    await connection.$connect()
+
+    await connection.item.deleteMany()
+  })
+
+  afterAll(async () => {
+    const { connection } = makeSut()
+    await connection.$disconnect()
+  })
+
+  it('should return an item by id', async () => {
+    const { sut, connection } = makeSut()
+    const { id: createdItemId } = await connection.item.create({
+      data: {
+        description: 'any_description',
+        price: 10,
+        depth: 1,
+        height: 1,
+        weight: 1,
+        width: 1
+      }
+    })
+
+    const item = await sut.getById(createdItemId)
+
+    expect(item).toBeTruthy()
+  })
+
+  it('should throw ItemNotFoundError if item does not exist', async () => {
+    const { sut } = makeSut()
+
+    const error = sut.getById('invalid_id')
+
+    await expect(error).rejects.toThrowError(new ItemNotFoundError({
+      targetProperty: 'id',
+      targetValue: 'invalid_id'
+    }))
+  })
+})
