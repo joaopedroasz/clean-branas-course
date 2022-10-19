0fdce334ff9b189a3a4ce547180d7812eada517a
João Pedro <joao.pedroasz00@gmail.com>
2022-10-19 18:52:12 -0300

    feat: ensure GetItemByIdRepository search item correctly

---
+++ src/infra/database/repositories/Item/GetByIdPostgresRepository.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client'
+
+import { Item } from '@/domain/entities'
+import { ItemNotFoundError } from '@/domain/errors'
+import { GetItemByIdRepository } from '@/domain/repositories/Item'
+
+export class GetItemByIdPostgresRepository implements GetItemByIdRepository {
+  private readonly connection: PrismaClient
+
+  constructor (connection: PrismaClient) {
+    this.connection = connection
+  }
+
+  async getById (id: string): Promise<Item> {
+    const item = await this.connection.item.findUnique({
+      where: {
+        id
+      }
+    })
+
+    if (!item) {
+      throw new ItemNotFoundError({
+        targetProperty: 'id',
+        targetValue: id
+      })
+    }
+
+    return new Item({
+      id: item.id,
+      description: item.description,
+      depthInCm: item.depth,
+      heightInCm: item.height,
+      price: item.price,
+      weightInKg: item.weight,
+      widthInCm: item.width
+    })
+  }
+}
