ae122d46d340930fd8f31112a827860977d1f58d
João Pedro <joao.pedroasz00@gmail.com>
2022-10-20 13:48:23 -0300

    refactor: remove discount column from coupon table

---
+++ src/infra/database/prisma/migrations/20221020164741_remove_discount_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `discount` on the `coupons` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "coupons" DROP COLUMN "discount";
