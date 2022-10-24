b74cbbb38eb52ec9c4dce8a7b27449475873bc50
João Pedro <joao.pedroasz00@gmail.com>
2022-10-24 16:09:57 -0300

    feat: ensure SearchOrderByCode calls GetOrderByCodeRepository correctly

--- src/application/UseCases/SearchOrderByCode.ts
+++ src/application/UseCases/SearchOrderByCode.ts
@@ -1,8 +1,17 @@
+import { GetOrderByCodeRepository } from '@/domain/repositories/Order'
 import { SearchOrderByCode } from '../contracts'
 import { SearchOrderByCodeInput, SearchOrderByCodeOutput } from '../DTOs'
 
 export class SearchOrderByCodeUseCase implements SearchOrderByCode {
+  private readonly getOrderByCodeRepository: GetOrderByCodeRepository
+
+  constructor (getOrderByCodeRepository: GetOrderByCodeRepository) {
+    this.getOrderByCodeRepository = getOrderByCodeRepository
+  }
+
   public async execute (input: SearchOrderByCodeInput): Promise<SearchOrderByCodeOutput> {
+    await this.getOrderByCodeRepository.getByCode(input.code)
+
     return {
       code: '202200000001',
       CPF: '483.967.454-04',
