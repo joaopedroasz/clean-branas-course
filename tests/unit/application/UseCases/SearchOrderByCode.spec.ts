5c0926fc5defa15c8534662926e475e2c92279a7
João Pedro <joao.pedroasz00@gmail.com>
2022-10-25 14:13:40 -0300

    test: ensure SearchOrderByCode throws if search for nonexistent item

--- tests/unit/application/UseCases/SearchOrderByCode.spec.ts
+++ tests/unit/application/UseCases/SearchOrderByCode.spec.ts
@@ -1,6 +1,8 @@
-import { Item, ItemProps, Order, OrderProps } from '@/domain/entities'
+import { Item, ItemProps, Order, OrderItem, OrderItemProps, OrderProps } from '@/domain/entities'
+import { ItemNotFoundError } from '@/domain/errors'
 import { GetOrderByCodeRepository } from '@/domain/repositories/Order'
 import { GetItemsByOrderCodeRepository } from '@/domain/repositories/Item'
+import { GetOrderItemsByOrderCodeRepository } from '@/domain/repositories/OrderItem'
 import { SearchOrderByCode } from '@/application/contracts'
 import { SearchOrderByCodeUseCase } from '@/application/UseCases/SearchOrderByCode'
 
@@ -8,6 +10,7 @@ type SutType = {
   sut: SearchOrderByCode
   getOrderByCodeRepository: GetOrderByCodeRepository
   getItemsByOrderCodeRepository: GetItemsByOrderCodeRepository
+  getOrderItemsByOrderCodeRepository: GetOrderItemsByOrderCodeRepository
 }
 
 const makeOrder = (props?: Partial<OrderProps>): Order => new Order({
@@ -28,6 +31,19 @@ const makeItem = (props?: Partial<ItemProps>): Item => new Item({
   ...props
 })
 
+const makeOrderItem = (props?: Partial<OrderItemProps>): OrderItem => new OrderItem({
+  itemId: 'any_id',
+  price: 1,
+  quantity: 1,
+  ...props
+})
+
+const makeGetOrderItemsByOrderCodeRepository = (): GetOrderItemsByOrderCodeRepository => ({
+  async getByOrderCode (code: string): Promise<OrderItem[]> {
+    return [makeOrderItem()]
+  }
+})
+
 const makeGetItemsByOrderCodeRepository = (): GetItemsByOrderCodeRepository => ({
   async getByCode (code: string): Promise<Item[]> {
     return [makeItem()]
@@ -43,15 +59,18 @@ const makeGetOrderByCodeRepository = (): GetOrderByCodeRepository => ({
 const makeSut = (): SutType => {
   const getOrderByCodeRepository = makeGetOrderByCodeRepository()
   const getItemsByOrderCodeRepository = makeGetItemsByOrderCodeRepository()
+  const getOrderItemsByOrderCodeRepository = makeGetOrderItemsByOrderCodeRepository()
   const sut = new SearchOrderByCodeUseCase(
     getOrderByCodeRepository,
-    getItemsByOrderCodeRepository
+    getItemsByOrderCodeRepository,
+    getOrderItemsByOrderCodeRepository
   )
 
   return {
     sut,
     getOrderByCodeRepository,
-    getItemsByOrderCodeRepository
+    getItemsByOrderCodeRepository,
+    getOrderItemsByOrderCodeRepository
   }
 }
 
@@ -95,13 +114,62 @@ describe('SearchOrderByCode UseCase', () => {
   })
 
   it('should call GetItemsByOrderCodeRepository correctly', async () => {
-    const { sut, getItemsByOrderCodeRepository } = makeSut()
+    const { sut, getItemsByOrderCodeRepository, getOrderItemsByOrderCodeRepository } = makeSut()
     const orderCode = '202200000003'
-    const items: Item[] = [makeItem({ id: '1' }), makeItem({ id: '2' }), makeItem({ id: '3' })]
-    const getByCodeSpy = vi.spyOn(getItemsByOrderCodeRepository, 'getByCode').mockResolvedValueOnce(items)
+    const itemsIds = ['1', '2', '3']
+    const items: Item[] = itemsIds.map(id => makeItem({ id }))
+    const getItemsByOrderCodeRepositorySpy = vi.spyOn(getItemsByOrderCodeRepository, 'getByCode').mockResolvedValueOnce(items)
+    const orderItems = itemsIds.map(id => makeOrderItem({ itemId: id }))
+    vi.spyOn(getOrderItemsByOrderCodeRepository, 'getByOrderCode').mockResolvedValueOnce(orderItems)
 
     await sut.execute({ code: orderCode })
 
-    expect(getByCodeSpy).toHaveBeenCalledWith(orderCode)
+    expect(getItemsByOrderCodeRepositorySpy).toHaveBeenCalledWith(orderCode)
+  })
+
+  it('should return same order items loaded by GetOrderItemsByOrderCodeRepository and GetItemsByOrderCodeRepository', async () => {
+    const { sut, getItemsByOrderCodeRepository, getOrderItemsByOrderCodeRepository } = makeSut()
+    const orderCode = '202200000004'
+    const itemsIds = ['1', '2', '3']
+    const items: Item[] = itemsIds.map(id => makeItem({ id }))
+    vi.spyOn(getItemsByOrderCodeRepository, 'getByCode').mockResolvedValueOnce(items)
+    const orderItems = itemsIds.map(id => makeOrderItem({ itemId: id }))
+    vi.spyOn(getOrderItemsByOrderCodeRepository, 'getByOrderCode').mockResolvedValueOnce(orderItems)
+
+    const result = await sut.execute({ code: orderCode })
+
+    expect(result.orderItems).toMatchObject(
+      orderItems.map(orderItem => {
+        const item = items.find(item => item.getId() === orderItem.getItemId())
+        return {
+          quantity: orderItem.getQuantity(),
+          item: {
+            id: item?.getId(),
+            description: item?.getDescription(),
+            price: item?.getPrice(),
+            weight: item?.getWeight(),
+            width: item?.getWidth(),
+            height: item?.getHeight(),
+            depth: item?.getDepth()
+          }
+        }
+      })
+    )
+  })
+
+  it('should throw ItemNotFoundError if GetOrderItemsByOrderCodeRepository has an item id that not exists', async () => {
+    const { sut, getOrderItemsByOrderCodeRepository, getItemsByOrderCodeRepository } = makeSut()
+    const orderCode = '202200000005'
+    const items: Item[] = [makeItem({ id: '1' }), makeItem({ id: '2' }), makeItem({ id: '3' })]
+    vi.spyOn(getItemsByOrderCodeRepository, 'getByCode').mockResolvedValueOnce(items)
+    const orderItems = [makeOrderItem({ itemId: '4' })]
+    vi.spyOn(getOrderItemsByOrderCodeRepository, 'getByOrderCode').mockResolvedValueOnce(orderItems)
+
+    const promise = sut.execute({ code: orderCode })
+
+    await expect(promise).rejects.toThrow(new ItemNotFoundError({
+      targetProperty: 'id',
+      targetValue: '4'
+    }))
   })
 })
