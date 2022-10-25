76f1be379ba93612c3acf37fe05ba26a5eea8204
João Pedro <joao.pedroasz00@gmail.com>
2022-10-25 15:17:33 -0300

    test: ensure GetOrderByCodeRepository returns valid order

---
+++ tests/integration/infra/database/repositories/Order/GetOrderByCodePostgresRepository.test.ts
@@ -0,0 +1,67 @@
+import { PrismaClient } from '@prisma/client'
+
+import { Order, OrderProps } from '@/domain/entities'
+import { OrderNotFoundError } from '@/domain/errors'
+import { GetOrderByCodeRepository } from '@/domain/repositories/Order'
+import { GetOrderByCodePostgresRepository } from '@/infra/database'
+
+const makeOrder = (props?: Partial<OrderProps>): Order => new Order({
+  buyerCPF: '67440503112',
+  sequence: 1,
+  purchaseDate: new Date('2022-10-25T14:25:00'),
+  ...props
+})
+
+type SutType = {
+  sut: GetOrderByCodeRepository
+  connection: PrismaClient
+}
+
+const connection = new PrismaClient()
+const makeSut = (): SutType => {
+  const sut = new GetOrderByCodePostgresRepository(connection)
+  return {
+    sut,
+    connection
+  }
+}
+
+describe('GetOrderByCodePostgresRepository', () => {
+  afterAll(async () => {
+    const { connection } = makeSut()
+    const deleteOrders = connection.order.deleteMany()
+    await connection.$transaction([deleteOrders])
+    await connection.$disconnect()
+  })
+
+  it('should return an existent order by code', async () => {
+    const { sut, connection } = makeSut()
+    const sequence = 0
+    const order = makeOrder({ sequence })
+
+    const { code: createdOrderCode } = await connection.order.create({
+      data: {
+        cpf: order.getCPF(),
+        code: order.getCode(),
+        total: order.getTotalPrice(),
+        sequence
+      }
+    })
+
+    const result = await sut.getByCode(createdOrderCode)
+
+    expect(result).toBeDefined()
+  })
+
+  it('should throw OrderNotFoundError if order does not exist', async () => {
+    const { sut } = makeSut()
+    const order = makeOrder()
+
+    const error = sut.getByCode(order.getCode())
+
+    await expect(error).rejects.toThrowError(new OrderNotFoundError({
+      targetProperty: 'code',
+      targetValue: order.getCode()
+    }))
+  })
+})
