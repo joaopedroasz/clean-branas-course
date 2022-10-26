34e90e878f2fda3558c3cfe9ff463882af10ecc9
João Pedro <joao.pedroasz00@gmail.com>
2022-10-26 15:38:40 -0300

    feat: ensure SearchOrdersByCPF calls GetOrderItemsByOrderCPFRepository correctly

--- src/application/UseCases/SearchOrdersByCPFUseCase.ts
+++ src/application/UseCases/SearchOrdersByCPFUseCase.ts
@@ -1,16 +1,24 @@
 import { GetOrdersByCPFRepository } from '@/domain/repositories/Order'
+import { GetOrderItemsByOrderCPFRepository } from '@/domain/repositories/OrderItem'
 import { SearchOrdersByCPF } from '../contracts'
 import { SearchOrdersByCPFInputDTO, SearchOrdersByCPFOutputDTO } from '../DTOs'
 
 export class SearchOrdersByCPFUseCase implements SearchOrdersByCPF {
   private readonly getOrdersByCPFRepository: GetOrdersByCPFRepository
+  private readonly getOrderItemsByOrderCPFRepository: GetOrderItemsByOrderCPFRepository
 
-  constructor (getOrdersByCPFRepository: GetOrdersByCPFRepository) {
+  constructor (
+    getOrdersByCPFRepository: GetOrdersByCPFRepository,
+    getOrderItemsByOrderCPFRepository: GetOrderItemsByOrderCPFRepository
+  ) {
     this.getOrdersByCPFRepository = getOrdersByCPFRepository
+    this.getOrderItemsByOrderCPFRepository = getOrderItemsByOrderCPFRepository
   }
 
   public async execute (input: SearchOrdersByCPFInputDTO): Promise<SearchOrdersByCPFOutputDTO> {
-    const orders = await this.getOrdersByCPFRepository.getByCPF(input.CPF)
+    const { CPF } = input
+    const orders = await this.getOrdersByCPFRepository.getByCPF(CPF)
+    await this.getOrderItemsByOrderCPFRepository.getByOrderCPF(CPF)
     return {
       orders: orders.map(order => ({
         code: order.getCode(),
