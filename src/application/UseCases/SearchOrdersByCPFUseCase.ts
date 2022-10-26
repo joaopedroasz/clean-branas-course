ed50d78f33f437497a21453e16fd63866f94e6d8
João Pedro <joao.pedroasz00@gmail.com>
2022-10-26 16:28:40 -0300

    feat: ensure SearchOrdersByCPF return entities loaded by repositories

--- src/application/UseCases/SearchOrdersByCPFUseCase.ts
+++ src/application/UseCases/SearchOrdersByCPFUseCase.ts
@@ -1,32 +1,68 @@
+import { Item, Order, OrderItem } from '@/domain/entities'
+import { ItemNotFoundError } from '@/domain/errors'
+import { GetItemsByOrderCPFRepository } from '@/domain/repositories/Item'
 import { GetOrdersByCPFRepository } from '@/domain/repositories/Order'
-import { GetOrderItemsByOrderCPFRepository } from '@/domain/repositories/OrderItem'
 import { SearchOrdersByCPF } from '../contracts'
-import { SearchOrdersByCPFInputDTO, SearchOrdersByCPFOutputDTO } from '../DTOs'
+import { SearchOrdersByCPFInputDTO, SearchOrdersByCPFOutputDTO, SearchOrdersByCPFOutputItem, SearchOrdersByCPFOutputOrder } from '../DTOs'
 
 export class SearchOrdersByCPFUseCase implements SearchOrdersByCPF {
   private readonly getOrdersByCPFRepository: GetOrdersByCPFRepository
-  private readonly getOrderItemsByOrderCPFRepository: GetOrderItemsByOrderCPFRepository
+  private readonly getItemsByOrderCPFRepository: GetItemsByOrderCPFRepository
 
   constructor (
     getOrdersByCPFRepository: GetOrdersByCPFRepository,
-    getOrderItemsByOrderCPFRepository: GetOrderItemsByOrderCPFRepository
+    getItemsByOrderCPFRepository: GetItemsByOrderCPFRepository
   ) {
     this.getOrdersByCPFRepository = getOrdersByCPFRepository
-    this.getOrderItemsByOrderCPFRepository = getOrderItemsByOrderCPFRepository
+    this.getItemsByOrderCPFRepository = getItemsByOrderCPFRepository
   }
 
   public async execute (input: SearchOrdersByCPFInputDTO): Promise<SearchOrdersByCPFOutputDTO> {
     const { CPF } = input
-    const orders = await this.getOrdersByCPFRepository.getByCPF(CPF)
-    await this.getOrderItemsByOrderCPFRepository.getByOrderCPF(CPF)
+    const [orders, items] = await Promise.all([
+      this.getOrdersByCPFRepository.getByCPF(CPF),
+      this.getItemsByOrderCPFRepository.getByOrderCPF(CPF)
+    ])
     return {
-      orders: orders.map(order => ({
-        code: order.getCode(),
-        CPF: order.getCPF(),
-        purchaseDate: order.getPurchaseDate(),
-        totalValue: order.getTotalPrice(),
-        orderItems: []
+      orders: this.formatOrders(orders, items)
+    }
+  }
+
+  private formatOrders (orders: Order[], items: Item[]): SearchOrdersByCPFOutputOrder[] {
+    return orders.map(order => ({
+      code: order.getCode(),
+      CPF: order.getCPF(),
+      purchaseDate: order.getPurchaseDate(),
+      totalValue: order.getTotalPrice(),
+      orderItems: order.getOrderItems().map(orderItem => ({
+        quantity: orderItem.getQuantity(),
+        item: this.getItemByOrderItem(orderItem, items)
       }))
+    }))
+  }
+
+  private getItemByOrderItem (orderItem: OrderItem, items: Item[]): SearchOrdersByCPFOutputItem {
+    const item = items.find(item => item.getId() === orderItem.getItemId())
+
+    if (!item) {
+      throw new ItemNotFoundError({
+        targetProperty: 'id',
+        targetValue: orderItem.getItemId()
+      })
+    }
+
+    return this.formatItem(item)
+  }
+
+  private formatItem (item: Item): SearchOrdersByCPFOutputItem {
+    return {
+      id: item.getId(),
+      description: item.getDescription(),
+      price: item.getPrice(),
+      weight: item.getWeight(),
+      height: item.getHeight(),
+      width: item.getWidth(),
+      depth: item.getDepth()
     }
   }
 }
