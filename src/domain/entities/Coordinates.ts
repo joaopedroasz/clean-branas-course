095237498fc158a3e76bc8de718cda0ee67c114e
João Pedro <joao.pedroasz00@gmail.com>
2022-10-27 16:47:55 -0300

    feat: ensure Coordinates throws if given longitude greater than 180

--- src/domain/entities/Coordinates.ts
+++ src/domain/entities/Coordinates.ts
@@ -1,4 +1,4 @@
-import { InvalidLatitudeError } from '../errors'
+import { InvalidLatitudeError, InvalidLongitudeError } from '../errors'
 
 export type CoordinatesProps = {
   latitude: number
@@ -17,9 +17,14 @@ export class Coordinates {
     this.longitude = longitude
 
     if (!this.isValidLatitude()) throw new InvalidLatitudeError(this.latitude)
+    if (!this.isValidLongitude()) throw new InvalidLongitudeError(this.longitude)
   }
 
   private isValidLatitude (): boolean {
     return this.latitude >= -90 && this.latitude <= 90
+  }
+
+  private isValidLongitude (): boolean {
+    return this.longitude < 180
   }
 }
