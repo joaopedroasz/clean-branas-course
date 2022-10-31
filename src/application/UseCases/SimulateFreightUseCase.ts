9d9863e57a0019932009f4b8fad44d77fc198794
João Pedro <joao.pedroasz00@gmail.com>
2022-10-31 17:08:20 -0300

    feat: ensure FreightSimulator calls GetCoordinateByCEP correctly

--- src/application/UseCases/SimulateFreightUseCase.ts
+++ src/application/UseCases/SimulateFreightUseCase.ts
@@ -1,17 +1,23 @@
 import { FreightCalculator } from '@/domain/entities'
+import { GetCoordinateByCEPGateway } from '@/domain/gateways/Coordinates'
 import { GetItemByIdRepository } from '@/domain/repositories/Item'
 import { SimulateFreight } from '@/application/contracts'
 import { SimulateFreightInputDTO, SimulateFreightOutputDTO } from '@/application/DTOs'
 
 export class SimulateFreightUseCase implements SimulateFreight {
   private readonly getItemByIdRepository: GetItemByIdRepository
+  private readonly getCoordinateByCEPGateway: GetCoordinateByCEPGateway
 
-  constructor (getItemByIdRepository: GetItemByIdRepository) {
+  constructor (
+    getItemByIdRepository: GetItemByIdRepository,
+    getCoordinateByCEPGateway: GetCoordinateByCEPGateway
+  ) {
     this.getItemByIdRepository = getItemByIdRepository
+    this.getCoordinateByCEPGateway = getCoordinateByCEPGateway
   }
 
   public async execute (input: SimulateFreightInputDTO): Promise<SimulateFreightOutputDTO> {
-    const { items } = input
+    const { items, destinationCEP } = input
     let total = 0
 
     for (const { itemId, quantity } of items) {
@@ -19,6 +25,8 @@ export class SimulateFreightUseCase implements SimulateFreight {
 
       total += new FreightCalculator({ item, quantity }).calculate()
     }
+
+    await this.getCoordinateByCEPGateway.getByCEP(destinationCEP)
 
     return { total }
   }
