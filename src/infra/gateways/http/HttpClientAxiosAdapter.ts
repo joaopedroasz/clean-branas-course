34c8fb3a575004ed88c92e7aa23c607a83e39d2d
João Pedro <joao.pedroasz00@gmail.com>
2022-11-01 16:23:12 -0300

    feat: ensure HttpClientAxiosAdapter returns axios response

--- src/infra/gateways/http/HttpClientAxiosAdapter.ts
+++ src/infra/gateways/http/HttpClientAxiosAdapter.ts
@@ -3,9 +3,9 @@ import axios from 'axios'
 import { HttpClient, RequestParams } from '../contracts'
 
 export class HttpClientAxiosAdapter implements HttpClient {
-  public async get <Response>(params: RequestParams): Promise<Response> {
-    await axios.get<Response>(params.url)
+  public async get <Response>({ url, params }: RequestParams): Promise<Response> {
+    const response = await axios.get<Response>(url, { params })
 
-    return {} as unknown as Response
+    return response.data
   }
 }
