4adf3d0c014096bffd5ffadf424b67dbd8ebb5b1
João Pedro <joao.pedroasz00@gmail.com>
2022-11-01 17:04:48 -0300

    feat: ensure HttpClientAxiosAdapter throws ExternalBadRequestError when status is 400

--- src/infra/gateways/http/HttpClientAxiosAdapter.ts
+++ src/infra/gateways/http/HttpClientAxiosAdapter.ts
@@ -1,11 +1,18 @@
 import axios from 'axios'
 
 import { HttpClient, RequestParams } from '../contracts'
+import { ExternalBadRequestError } from '../errors'
 
 export class HttpClientAxiosAdapter implements HttpClient {
   public async get <Response>({ url, params }: RequestParams): Promise<Response> {
-    const response = await axios.get<Response>(url, { params })
+    try {
+      const response = await axios.get<Response>(url, { params })
 
-    return response.data
+      return response.data
+    } catch (error) {
+      if (!axios.isAxiosError(error)) throw error
+
+      throw new ExternalBadRequestError(error.response?.data)
+    }
   }
 }
