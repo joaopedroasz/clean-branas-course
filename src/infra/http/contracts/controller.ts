5d70407a2c8d8b14b99117d748446d1c428948be
João Pedro <joao.pedroasz00@gmail.com>
2022-11-03 11:10:23 -0300

    feat: create base Controller contracts

---
+++ src/infra/http/contracts/controller.ts
@@ -0,0 +1,7 @@
+import { HttpResponse } from './http'
+
+type Default = Record<string, any>
+
+export interface HttpController<Request = Default, ResponseBody = Default> {
+  handle: (request: Request) => Promise<HttpResponse<ResponseBody>>
+}
---
+++ src/infra/http/contracts/index.ts
@@ -0,0 +1,3 @@
+export * from './controller'
+export * from './httpRequest'
+export * from './http'
