457e922fffea92fecb201bc178ba05851b71975f
João Pedro <joao.pedroasz00@gmail.com>
2022-11-03 16:05:51 -0300

    feat: ensure SimulateFreightHttp calls use case correctly

--- src/infra/http/controllers/SimulateFreightHttp.ts
+++ src/infra/http/controllers/SimulateFreightHttp.ts
@@ -1,12 +1,24 @@
+import { SimulateFreight } from '@/application/contracts'
+import { SimulateFreightInputDTO } from '@/application/DTOs'
+
 import { HttpResponse, SimulateFreightHttp } from '../contracts'
 import { SimulateFreightHttpInputDTO, SimulateFreightHttpOutputDTO } from '../DTOs'
 import { MissingParamError } from '../errors'
 import { badRequest, ok } from '../helpers'
 
 export class SimulateFreightHttpController implements SimulateFreightHttp {
+  private readonly simulateFreight: SimulateFreight
+
+  constructor (simulateFreight: SimulateFreight) {
+    this.simulateFreight = simulateFreight
+  }
+
   public async handle (request: SimulateFreightHttpInputDTO): Promise<HttpResponse<SimulateFreightHttpOutputDTO | Error>> {
     const error = this.validateRequest(request)
     if (error) return badRequest(error)
+
+    const simulateFreightInput = this.formatRequest(request)
+    await this.simulateFreight.execute(simulateFreightInput)
 
     return ok<SimulateFreightHttpOutputDTO>({
       freight: 0
@@ -17,5 +29,17 @@ export class SimulateFreightHttpController implements SimulateFreightHttp {
     const { cep, items } = request
     if (!cep) return new MissingParamError('cep')
     if (!items.length) return new MissingParamError('items')
+  }
+
+  private formatRequest (request: SimulateFreightHttpInputDTO): SimulateFreightInputDTO {
+    const { cep, items } = request
+
+    return {
+      destinationCEP: cep,
+      items: items.map(item => ({
+        itemId: item.item_id,
+        quantity: item.quantity
+      }))
+    }
   }
 }
