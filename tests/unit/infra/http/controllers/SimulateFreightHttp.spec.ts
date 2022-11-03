eb8e5f4784ccb66945f1d10db44d0757cbb03b32
João Pedro <joao.pedroasz00@gmail.com>
2022-11-03 14:49:46 -0300

    test: ensure SimulateFreightHttp returns 400 if no CEP provided

---
+++ tests/unit/infra/http/controllers/SimulateFreightHttp.spec.ts
@@ -0,0 +1,33 @@
+import { SimulateFreightHttp, SimulateFreightHttpController, SimulateFreightHttpInputDTO } from '@/infra/http'
+import { MissingParamError } from '@/infra/http/errors'
+
+type SutType = {
+  sut: SimulateFreightHttp
+}
+
+const makeSut = (): SutType => {
+  const sut = new SimulateFreightHttpController()
+  return {
+    sut
+  }
+}
+
+describe('SimulateFreightHttpController', () => {
+  it('should return badRequest if no CEP provided', async () => {
+    const { sut } = makeSut()
+    const request: SimulateFreightHttpInputDTO = {
+      cep: '',
+      items: [
+        {
+          item_id: 'any_item_id',
+          quantity: 1
+        }
+      ]
+    }
+
+    const response = await sut.handle(request)
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new MissingParamError('cep'))
+  })
+})
