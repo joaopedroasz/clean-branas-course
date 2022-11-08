9844d378b1d7f4da8f39b42b460422f762410568
João Pedro <joao.pedroasz00@gmail.com>
2022-11-08 17:28:49 -0300

    refactor: change Freight revolser to all resolvers

--- src/infra/http/graphql/resolvers/freight.ts
+++ src/infra/http/graphql/resolvers/freight.ts
@@ -3,19 +3,21 @@ import { GetItemByIdPrismaRepository, PrismaClientSingleton } from '@/infra/data
 import { GetCoordinatesByCEPGatewayBrasilAPIAdapter, HttpClientAxiosAdapter } from '@/infra/gateways'
 import { SimulateFreightHttpController } from '../../controllers'
 import { adaptResolver } from '../adapters'
-import { QueryResolvers } from '../generated/generated-resolvers'
+import { Resolvers } from '../generated/generated-resolvers'
 
-export const freightResolvers: QueryResolvers = {
-  simulateFreight: async (_, args) => {
-    const httpClient = new HttpClientAxiosAdapter()
-    const getCoordinatesByCEPHttpClient = new GetCoordinatesByCEPGatewayBrasilAPIAdapter('https://brasilapi.com.br/api/cep/v2', httpClient)
+export const freightResolvers: Resolvers = {
+  Query: {
+    simulateFreight: async (_, args) => {
+      const httpClient = new HttpClientAxiosAdapter()
+      const getCoordinatesByCEPHttpClient = new GetCoordinatesByCEPGatewayBrasilAPIAdapter('https://brasilapi.com.br/api/cep/v2', httpClient)
 
-    const prismaClient = PrismaClientSingleton.getInstance()
-    const getItemByIdPrismRepository = new GetItemByIdPrismaRepository(prismaClient)
+      const prismaClient = PrismaClientSingleton.getInstance()
+      const getItemByIdPrismRepository = new GetItemByIdPrismaRepository(prismaClient)
 
-    const simulateFreight = new SimulateFreightUseCase(getItemByIdPrismRepository, getCoordinatesByCEPHttpClient)
-    const simulateFreightController = new SimulateFreightHttpController(simulateFreight)
+      const simulateFreight = new SimulateFreightUseCase(getItemByIdPrismRepository, getCoordinatesByCEPHttpClient)
+      const simulateFreightController = new SimulateFreightHttpController(simulateFreight)
 
-    return adaptResolver(simulateFreightController, args)
+      return adaptResolver(simulateFreightController, args)
+    }
   }
 }
