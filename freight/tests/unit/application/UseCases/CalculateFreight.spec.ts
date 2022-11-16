521553ab740a899eba389cf74ef55330356f9c2d
João Pedro <joao.pedroasz00@gmail.com>
2022-11-16 17:08:44 -0300

    test: ensure CalculateFreightUseCase call freightCalculator with orderItems data

--- freight/tests/unit/application/UseCases/CalculateFreight.spec.ts
+++ freight/tests/unit/application/UseCases/CalculateFreight.spec.ts
@@ -1,4 +1,4 @@
-import { City, DistanceCalculator } from '@/domain/entities'
+import { City, DistanceCalculator, FreightCalculator } from '@/domain/entities'
 import { GetCityByZipCodeRepository } from '@/domain/repositories/City'
 import { CalculateFreight } from '@/application/contracts'
 import { CalculateFreightUseCase } from '@/application/UseCases'
@@ -27,6 +27,14 @@ const makeSut = (): SutType => {
 }
 
 describe('CalculateFreight Use Case', () => {
+  beforeEach(() => {
+    vitest.spyOn(DistanceCalculator.prototype, 'calculate').mockReturnValueOnce(10)
+  })
+
+  afterAll(() => {
+    vitest.restoreAllMocks()
+  })
+
   it('should call GetCityByZipCode twice with correct params', async () => {
     const { sut, getCityByZipCodeRepository } = makeSut()
     const getCityByZipCodeSpy = vitest.spyOn(getCityByZipCodeRepository, 'getByZipCode')
@@ -50,7 +58,7 @@ describe('CalculateFreight Use Case', () => {
 
   it('should call DistanceCalculator with the results from GetCityByZipCode', async () => {
     const { sut } = makeSut()
-    const calculateFreightSpy = vitest.spyOn(DistanceCalculator.prototype, 'calculate')
+    const distanceCalculatorSpy = vitest.spyOn(DistanceCalculator.prototype, 'calculate')
     await sut.execute({
       from: '11111-111',
       to: '22222-222',
@@ -63,6 +71,29 @@ describe('CalculateFreight Use Case', () => {
       ]
     })
 
-    expect(calculateFreightSpy).toHaveBeenCalledTimes(1)
+    expect(distanceCalculatorSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call FreightCalculator with orderItems data', async () => {
+    const { sut } = makeSut()
+    const freightCalculatorSpy = vitest.spyOn(FreightCalculator.prototype, 'calculate')
+    await sut.execute({
+      from: '11111-111',
+      to: '22222-222',
+      orderItems: [
+        {
+          volume: 1,
+          density: 2,
+          quantity: 3
+        },
+        {
+          volume: 4,
+          density: 5,
+          quantity: 6
+        }
+      ]
+    })
+
+    expect(freightCalculatorSpy).toHaveBeenCalledTimes(2)
   })
 })
