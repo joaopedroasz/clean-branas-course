9aa024632a29096c815e74105729b374becbfdda
João Pedro <joao.pedroasz00@gmail.com>
2022-11-17 18:23:58 -0300

    test: add discount and code methods to OrderCoupon entity

--- checkout/tests/unit/domain/OrderCoupon.spec.ts
+++ checkout/tests/unit/domain/OrderCoupon.spec.ts
@@ -17,4 +17,20 @@ describe('OrderCoupon', () => {
   it('should throw InvalidPercentageError if percentage is less than 0', () => {
     expect(() => makeSut({ percentage: -1 })).toThrowError(new InvalidPercentageError(-1))
   })
+
+  it('should calculate discount', () => {
+    const sut = makeSut()
+
+    const discount = sut.calculatePriceDiscount(100)
+
+    expect(discount).toBe(90)
+  })
+
+  it('should return percentage discount', () => {
+    const sut = makeSut()
+
+    const percentageDiscount = sut.getPercentageDiscount(350)
+
+    expect(percentageDiscount).toBe(35)
+  })
 })
