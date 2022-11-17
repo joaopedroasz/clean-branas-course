6823a729f16bbf2e8b5d04a9f49c50f8a80a1184
João Pedro <joao.pedroasz00@gmail.com>
2022-11-17 17:41:06 -0300

    test: ensure OrderCoupon throws if given percentage is less than 0

--- checkout/tests/unit/domain/OrderCoupon.spec.ts
+++ checkout/tests/unit/domain/OrderCoupon.spec.ts
@@ -1,8 +1,10 @@
 import { OrderCoupon, OrderCouponProps } from '@/domain/entities'
+import { InvalidPercentageError } from '@/domain/errors'
 
 const makeSut = (props?: Partial<OrderCouponProps>): OrderCoupon => new OrderCoupon({
   code: 'any_code',
-  percentage: 10
+  percentage: 10,
+  ...props
 })
 
 describe('OrderCoupon', () => {
@@ -10,5 +12,9 @@ describe('OrderCoupon', () => {
     const sut = makeSut()
 
     expect(sut).toBeInstanceOf(OrderCoupon)
+  })
+
+  it('should throw InvalidPercentageError if percentage is less than 0', () => {
+    expect(() => makeSut({ percentage: -1 })).toThrowError(new InvalidPercentageError(-1))
   })
 })
