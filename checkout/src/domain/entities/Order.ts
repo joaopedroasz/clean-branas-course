665a38342c8512c87dfeee02b22775a55f9328d6
João Pedro <joao.pedroasz00@gmail.com>
2022-11-17 18:33:36 -0300

    feat: remove freight from price calculation

--- checkout/src/domain/entities/Order.ts
+++ checkout/src/domain/entities/Order.ts
@@ -1,15 +1,16 @@
 import { ForbiddenAddDuplicatedItemError } from '@/domain/errors'
 import { Coupon } from './Coupon'
 import { CPF } from './CPF'
-import { FreightCalculator } from './FreightCalculator'
 import { Item } from './Item'
 import { OrderCode } from './OrderCode'
+import { OrderCoupon } from './OrderCoupon'
 import { OrderItem } from './OrderItem'
 
 export type OrderProps = {
   buyerCPF: string
   purchaseDate?: Date
   sequence: number
+  freight?: number
 }
 
 export type AddItemProps = {
@@ -21,21 +22,26 @@ export class Order {
   private readonly buyerCPF: CPF
   private readonly orderItems: OrderItem[]
   private readonly purchaseDate: Date
-  private coupon?: Coupon
-  private freight: number
+  private coupon?: OrderCoupon
   private readonly code: OrderCode
+  private readonly freight: number
 
   constructor ({
     buyerCPF,
     purchaseDate = new Date(),
-    sequence
+    sequence,
+    freight = 0
   }: OrderProps) {
     this.buyerCPF = new CPF(buyerCPF)
     this.orderItems = []
     this.coupon = undefined
-    this.freight = 0
     this.purchaseDate = purchaseDate
     this.code = new OrderCode({ date: purchaseDate, sequence })
+    this.freight = freight
+  }
+
+  public getFreight (): number {
+    return this.freight
   }
 
   public getOrderItems (): OrderItem[] {
@@ -58,10 +64,6 @@ export class Order {
     return this.purchaseDate
   }
 
-  public getFreightPrice (): number {
-    return this.freight
-  }
-
   public addItem ({ item, quantity }: AddItemProps): void {
     if (this.alreadyHasItem(item.getId())) throw new ForbiddenAddDuplicatedItemError(item.getId())
     this.orderItems.push(new OrderItem({
@@ -69,7 +71,6 @@ export class Order {
       price: item.getPrice(),
       quantity
     }))
-    this.freight += new FreightCalculator({ item, quantity, distanceInKm: 1000 }).calculate()
   }
 
   private alreadyHasItem (itemId: string): boolean {
@@ -78,11 +79,11 @@ export class Order {
 
   public addCoupon (coupon: Coupon): void {
     if (coupon.isExpired(this.purchaseDate)) return
-    this.coupon = coupon
+    this.coupon = coupon.createOrderCoupon()
   }
 
   public getTotalPrice (): number {
-    let totalPrice = this.freight
+    let totalPrice = 0
     for (const orderItem of this.orderItems) {
       totalPrice += orderItem.calculatePrice()
     }
