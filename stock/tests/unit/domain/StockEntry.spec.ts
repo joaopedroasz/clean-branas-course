1c55ed7951376fa4f85800f6682192ac7d44f71e
João Pedro <joao.pedroasz00@gmail.com>
2022-12-03 19:00:50 -0300

    test: ensure StockEntry throws if negative quantity provided

--- stock/tests/unit/domain/StockEntry.spec.ts
+++ stock/tests/unit/domain/StockEntry.spec.ts
@@ -1,4 +1,5 @@
 import { StockEntry, StockEntryProps } from '@/domain/models'
+import { InvalidQuantityError } from '@/domain/errors'
 
 const makeSut = (props?: Partial<StockEntryProps>): StockEntry => new StockEntry({
   id: 'any_id',
@@ -13,5 +14,11 @@ describe('StockEntry Entity', () => {
     const sut = makeSut()
 
     expect(sut).toBeTruthy()
+  })
+
+  it('should throw InvalidQuantityError if quantity is less than 0', () => {
+    const sut = (): StockEntry => makeSut({ quantity: -1 })
+
+    expect(sut).toThrowError(new InvalidQuantityError(-1))
   })
 })
