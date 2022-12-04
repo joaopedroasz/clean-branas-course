7a0cc25394456ac203c3091c268cb7ff01c0d0e4
João Pedro <joao.pedroasz00@gmail.com>
2022-12-04 13:24:41 -0300

    feat: ensure DecreaseStock calls StockCalculator correctly

--- stock/src/application/UseCases/DecreaseStock.ts
+++ stock/src/application/UseCases/DecreaseStock.ts
@@ -1,18 +1,20 @@
-import { GetStockEntriesByItemId } from '@/domain/repositories'
+import { StockCalculator, StockEntry } from '@/domain/models'
+import { GetStockEntriesByItemIdRepository } from '@/domain/repositories'
 import { DecreaseStock, DecreaseStockInput, DecreaseStockOutput } from '../contracts'
 import { EmptyStockError } from '../errors'
 
 export class DecreaseStockUseCase implements DecreaseStock {
-  private readonly getStockEntriesByItemId: GetStockEntriesByItemId
+  private readonly getStockEntriesByItemIdRepository: GetStockEntriesByItemIdRepository
 
-  constructor (getStockEntriesByItemId: GetStockEntriesByItemId) {
-    this.getStockEntriesByItemId = getStockEntriesByItemId
+  constructor (getStockEntriesByItemIdRepository: GetStockEntriesByItemIdRepository) {
+    this.getStockEntriesByItemIdRepository = getStockEntriesByItemIdRepository
   }
 
-  async execute ({ itemId }: DecreaseStockInput): Promise<DecreaseStockOutput> {
-    const stockEntries = await this.getStockEntriesByItemId.getByItemId(itemId)
-
+  async execute ({ itemId, decreaseQuantity }: DecreaseStockInput): Promise<DecreaseStockOutput> {
+    const stockEntries = await this.getStockEntriesByItemIdRepository.getByItemId(itemId)
     if (!stockEntries.length) throw new EmptyStockError(itemId)
+    const removingStockEntry = new StockEntry({ itemId, quantity: decreaseQuantity, operation: 'remove' })
+    new StockCalculator([...stockEntries, removingStockEntry]).calculate()
 
     return {
       amountInStock: 0,
