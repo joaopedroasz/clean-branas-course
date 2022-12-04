b11898d20ce7dd812c24638aeeff28b23c19ab1d
João Pedro <joao.pedroasz00@gmail.com>
2022-12-04 13:24:55 -0300

    test: ensure DecreaseStock calls StockCalculator correctly

--- stock/tests/unit/application/UseCases/DecreaseStock.spec.ts
+++ stock/tests/unit/application/UseCases/DecreaseStock.spec.ts
@@ -1,5 +1,5 @@
-import { StockEntry, StockEntryProps } from '@/domain/models'
-import { GetStockEntriesByItemId } from '@/domain/repositories'
+import { StockCalculator, StockEntry, StockEntryProps } from '@/domain/models'
+import { GetStockEntriesByItemIdRepository } from '@/domain/repositories'
 import { DecreaseStock } from '@/application/contracts'
 import { DecreaseStockUseCase } from '@/application/UseCases'
 import { EmptyStockError } from '@/application/errors'
@@ -12,28 +12,31 @@ const makeStockEntry = (props?: Partial<StockEntryProps>): StockEntry => new Sto
   ...props
 })
 
-const makeGetStockEntriesByItemId = (): GetStockEntriesByItemId => ({
-  getByItemId: async (itemId: string): Promise<StockEntry[]> => ([makeStockEntry()])
+const makeGetStockEntriesByItemId = (): GetStockEntriesByItemIdRepository => ({
+  getByItemId: async (itemId: string): Promise<StockEntry[]> => ([
+    makeStockEntry({ operation: 'add', quantity: 10 }),
+    makeStockEntry({ operation: 'remove', quantity: 3 })
+  ])
 })
 
 type SutType = {
   sut: DecreaseStock
-  getStockEntriesByItemId: GetStockEntriesByItemId
+  getStockEntriesByItemIdRepository: GetStockEntriesByItemIdRepository
 }
 
 const makeSut = (): SutType => {
-  const getStockEntriesByItemId = makeGetStockEntriesByItemId()
-  const sut = new DecreaseStockUseCase(getStockEntriesByItemId)
+  const getStockEntriesByItemIdRepository = makeGetStockEntriesByItemId()
+  const sut = new DecreaseStockUseCase(getStockEntriesByItemIdRepository)
   return {
     sut,
-    getStockEntriesByItemId
+    getStockEntriesByItemIdRepository
   }
 }
 
 describe('DecreaseStockUseCase', () => {
   it('should call GetStockEntriesByItemId with correct itemId', async () => {
-    const { sut, getStockEntriesByItemId } = makeSut()
-    const getStockEntriesByItemIdSpy = vi.spyOn(getStockEntriesByItemId, 'getByItemId')
+    const { sut, getStockEntriesByItemIdRepository } = makeSut()
+    const getStockEntriesByItemIdSpy = vi.spyOn(getStockEntriesByItemIdRepository, 'getByItemId')
 
     await sut.execute({
       itemId: 'any_item_id',
@@ -44,11 +47,22 @@ describe('DecreaseStockUseCase', () => {
   })
 
   it('should throw EmptyStockError if there is no stock entries for the given itemId', async () => {
-    const { sut, getStockEntriesByItemId } = makeSut()
-    vi.spyOn(getStockEntriesByItemId, 'getByItemId').mockResolvedValueOnce([])
+    const { sut, getStockEntriesByItemIdRepository } = makeSut()
+    vi.spyOn(getStockEntriesByItemIdRepository, 'getByItemId').mockResolvedValueOnce([])
 
     const error = sut.execute({ decreaseQuantity: 1, itemId: 'item_no_stock' })
 
     await expect(error).rejects.toThrow(new EmptyStockError('item_no_stock'))
+  })
+
+  it('should call StockCalculator.calculate', async () => {
+    const { sut, getStockEntriesByItemIdRepository } = makeSut()
+    const stockEntries = [makeStockEntry({ operation: 'add', quantity: 10 }), makeStockEntry({ operation: 'remove', quantity: 2 })]
+    vi.spyOn(getStockEntriesByItemIdRepository, 'getByItemId').mockResolvedValueOnce(stockEntries)
+    const stockCalculatorSpy = vi.spyOn(StockCalculator.prototype, 'calculate')
+
+    await sut.execute({ decreaseQuantity: 1, itemId: 'any_item_id' })
+
+    expect(stockCalculatorSpy).toHaveBeenCalled()
   })
 })
