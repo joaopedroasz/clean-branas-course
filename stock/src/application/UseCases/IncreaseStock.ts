416543858d7f5d7ee9a4aff6eb915330797b2b98
João Pedro <joao.pedroasz00@gmail.com>
2022-12-05 12:10:46 -0300

    feat: ensure IncreaseStock calls StockCalculator with correct params

--- stock/src/application/UseCases/IncreaseStock.ts
+++ stock/src/application/UseCases/IncreaseStock.ts
@@ -1,3 +1,4 @@
+import { StockCalculator, StockEntry } from '@/domain/models'
 import { GetStockEntriesByItemIdRepository } from '@/domain/repositories'
 import { IncreaseStock, IncreaseStockInput, IncreaseStockOutput } from '../contracts'
 
@@ -8,8 +9,10 @@ export class IncreaseStockUseCase implements IncreaseStock {
     this.getStockEntriesByItemIdRepository = getStockEntriesByItemIdRepository
   }
 
-  public async execute ({ itemId }: IncreaseStockInput): Promise<IncreaseStockOutput> {
-    await this.getStockEntriesByItemIdRepository.getByItemId(itemId)
+  public async execute ({ itemId, quantity }: IncreaseStockInput): Promise<IncreaseStockOutput> {
+    const stockEntries = await this.getStockEntriesByItemIdRepository.getByItemId(itemId)
+    const addingStock = new StockEntry({ itemId, quantity, operation: 'add' })
+    new StockCalculator([...stockEntries, addingStock]).calculate()
     return {
       amountInStock: 0,
       itemId: ''
