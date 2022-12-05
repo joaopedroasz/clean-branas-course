7db60e7348dd55669a65424d0ca1403bdeb1ec94
João Pedro <joao.pedroasz00@gmail.com>
2022-12-05 14:05:52 -0300

    test: ensure PrismaConnectionSingleton returns same PrismaConnection

--- stock/tests/integration/infra/database/PrismaConnectionSingleton.test.ts
+++ stock/tests/integration/infra/database/PrismaConnectionSingleton.test.ts
@@ -14,4 +14,23 @@ describe('PrismaConnectionSingleton', () => {
 
     expect(sut).toBe(PrismaConnectionSingleton.getInstance())
   })
+
+  it('should return only one instance of PrismaClient', () => {
+    const { sut } = makeSut()
+
+    const client = sut.getClient()
+
+    expect(client).toBe(sut.getClient())
+  })
+
+  it('should return same PrismaClient in different instances of PrismaConnectionSingleton', () => {
+    const { sut } = makeSut()
+
+    const client = sut.getClient()
+
+    const sut2 = PrismaConnectionSingleton.getInstance()
+    const client2 = sut2.getClient()
+
+    expect(client).toBe(client2)
+  })
 })
