3aa4c23ee7cecd3b744f867f3a0d125374c33cd8
João Pedro <joao.pedroasz00@gmail.com>
2022-12-05 12:16:04 -0300

    feat: ensure IncreaseStock calls SaveStockEntryRepository with correct params

--- stock/src/application/UseCases/IncreaseStock.ts
+++ stock/src/application/UseCases/IncreaseStock.ts
@@ -1,18 +1,24 @@
 import { StockCalculator, StockEntry } from '@/domain/models'
-import { GetStockEntriesByItemIdRepository } from '@/domain/repositories'
+import { GetStockEntriesByItemIdRepository, SaveStockEntryRepository } from '@/domain/repositories'
 import { IncreaseStock, IncreaseStockInput, IncreaseStockOutput } from '../contracts'
 
 export class IncreaseStockUseCase implements IncreaseStock {
   private readonly getStockEntriesByItemIdRepository: GetStockEntriesByItemIdRepository
+  private readonly saveStockEntryRepository: SaveStockEntryRepository
 
-  constructor (getStockEntriesByItemIdRepository: GetStockEntriesByItemIdRepository) {
+  constructor (
+    getStockEntriesByItemIdRepository: GetStockEntriesByItemIdRepository,
+    saveStockEntryRepository: SaveStockEntryRepository
+  ) {
     this.getStockEntriesByItemIdRepository = getStockEntriesByItemIdRepository
+    this.saveStockEntryRepository = saveStockEntryRepository
   }
 
   public async execute ({ itemId, quantity }: IncreaseStockInput): Promise<IncreaseStockOutput> {
     const stockEntries = await this.getStockEntriesByItemIdRepository.getByItemId(itemId)
     const addingStock = new StockEntry({ itemId, quantity, operation: 'add' })
     new StockCalculator([...stockEntries, addingStock]).calculate()
+    await this.saveStockEntryRepository.save(addingStock)
     return {
       amountInStock: 0,
       itemId: ''
