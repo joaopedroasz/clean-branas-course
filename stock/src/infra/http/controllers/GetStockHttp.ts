ac3932e671411e483b5207a2170409c275b90173
João Pedro <joao.pedroasz00@gmail.com>
2022-12-07 11:28:33 -0300

    feat: ensure GetStockHttpController calls GetStock with correct params

--- stock/src/infra/http/controllers/GetStockHttp.ts
+++ stock/src/infra/http/controllers/GetStockHttp.ts
@@ -1,11 +1,21 @@
+import { GetStock } from '@/application/contracts'
 import { GetStockHttp, GetStockHttpInput, GetStockHttpOutput, HttpResponse } from '../contracts'
 import { MissingParamError } from '../errors'
 import { badRequest } from '../helpers'
 
 export class GetStockHttpController implements GetStockHttp {
+  private readonly getStock: GetStock
+
+  constructor (getStock: GetStock) {
+    this.getStock = getStock
+  }
+
   public async handle (request: GetStockHttpInput): Promise<HttpResponse<GetStockHttpOutput | Error >> {
     const errorInRequest = this.validateRequest(request)
     if (errorInRequest) return badRequest(errorInRequest)
+
+    const { itemId } = request
+    await this.getStock.execute({ itemId })
 
     return {
       statusCode: 0,
