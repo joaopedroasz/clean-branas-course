ccd24ebc4bd3e0fc1600be3f5880db03ad17b203
João Pedro <joao.pedroasz00@gmail.com>
2022-12-10 15:29:21 -0300

    feat: ensure DecreaseStockHttpController calls use case with correct values

--- stock/src/infra/http/controllers/DecreaseStockHttp.ts
+++ stock/src/infra/http/controllers/DecreaseStockHttp.ts
@@ -1,11 +1,21 @@
+import { DecreaseStock } from '@/application/contracts'
 import { DecreaseStockHttp, DecreaseStockHttpInput, DecreaseStockHttpOutput, HttpResponse } from '../contracts'
 import { MissingParamError } from '../errors'
 import { badRequest } from '../helpers'
 
 export class DecreaseStockHttpController implements DecreaseStockHttp {
+  private readonly decreaseStock: DecreaseStock
+
+  constructor (decreaseStock: DecreaseStock) {
+    this.decreaseStock = decreaseStock
+  }
+
   public async handle (request: DecreaseStockHttpInput): Promise<HttpResponse<DecreaseStockHttpOutput | Error>> {
     const requestError = this.validateRequest(request)
     if (requestError) return badRequest(requestError)
+
+    const { itemId, quantity } = request
+    await this.decreaseStock.execute({ itemId, decreaseQuantity: quantity })
 
     return {
       statusCode: 0,
