4042a95c60e24f7ae232dfa9c2acd1e31ddeaf12
João Pedro <joao.pedroasz00@gmail.com>
2022-12-10 15:22:51 -0300

    test: ensure DecreaseStockHttpController returns badRequest if itemId not provided

---
+++ stock/tests/unit/infra/http/controllers/DecreaseStockHttp.spec.ts
@@ -0,0 +1,35 @@
+import {
+  badRequest,
+  DecreaseStockHttp,
+  DecreaseStockHttpController,
+  DecreaseStockHttpInput,
+  MissingParamError
+} from '@/infra/http'
+
+const makeRequest = (props?: Partial<DecreaseStockHttpInput>): DecreaseStockHttpInput => ({
+  itemId: 'any_id',
+  quantity: 1,
+  ...props
+})
+
+type SutType = {
+  sut: DecreaseStockHttp
+}
+
+const makeSut = (): SutType => {
+  const sut = new DecreaseStockHttpController()
+  return {
+    sut
+  }
+}
+
+describe('DecreaseStockHttpController', () => {
+  it('should return badRequest if itemId not provided', async () => {
+    const { sut } = makeSut()
+    const request = makeRequest({ itemId: '' })
+
+    const response = await sut.handle(request)
+
+    expect(response).toEqual(badRequest(new MissingParamError('itemId')))
+  })
+})
