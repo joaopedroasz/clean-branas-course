c57dbb98f83b4504b13cfac6f9e554cb80e5976e
João Pedro <joao.pedroasz00@gmail.com>
2022-12-27 12:18:12 -0300

    feat: ensure GetItemsByIdsHttpController returns serverError if GetItemsByIds throws

--- catalog/src/infra/http/controllers/GetItemsByIdsHttp.ts
+++ catalog/src/infra/http/controllers/GetItemsByIdsHttp.ts
@@ -1,7 +1,7 @@
 import { GetItemsByIds } from '@/application/contracts'
 import { GetItemsByIdsHttp, GetItemsByIdsHttpInput, GetItemsByIdsHttpOutput, HttpResponse } from '../contracts'
 import { MissingParamError } from '../errors'
-import { badRequest, ok } from '../helpers'
+import { badRequest, ok, serverError, unknownError } from '../helpers'
 
 export class GetItemsByIdsHttpController implements GetItemsByIdsHttp {
   private readonly getItemsByIds: GetItemsByIds
@@ -11,10 +11,17 @@ export class GetItemsByIdsHttpController implements GetItemsByIdsHttp {
   }
 
   public async handle ({ itemsIds }: GetItemsByIdsHttpInput): Promise<HttpResponse<GetItemsByIdsHttpOutput | Error>> {
-    if (!itemsIds) return badRequest(new MissingParamError('itemsIds'))
+    try {
+      if (!itemsIds) return badRequest(new MissingParamError('itemsIds'))
 
-    const output = await this.getItemsByIds.execute({ ids: itemsIds.split(',') })
+      const output = await this.getItemsByIds.execute({ ids: itemsIds.split(',') })
 
-    return ok<GetItemsByIdsHttpOutput>(output)
+      return ok<GetItemsByIdsHttpOutput>(output)
+    } catch (error) {
+      const isError = error instanceof Error
+      if (!isError) return unknownError(error)
+
+      return serverError(error)
+    }
   }
 }
