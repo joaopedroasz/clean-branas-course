a85316506bf21deaf4e3ed2fb2f69fcc49329474
João Pedro <joao.pedroasz00@gmail.com>
2022-12-27 12:05:52 -0300

    feat: ensure GetItemsByIdsHttpController calss GetItemsByIds with correct params

--- catalog/src/infra/http/controllers/GetItemsByIdsHttp.ts
+++ catalog/src/infra/http/controllers/GetItemsByIdsHttp.ts
@@ -1,10 +1,19 @@
+import { GetItemsByIds } from '@/application/contracts'
 import { GetItemsByIdsHttp, GetItemsByIdsHttpInput, GetItemsByIdsHttpOutput } from '../contracts'
 import { MissingParamError } from '../errors'
 import { badRequest } from '../helpers'
 
 export class GetItemsByIdsHttpController implements GetItemsByIdsHttp {
+  private readonly getItemsByIds: GetItemsByIds
+
+  constructor (getItemsByIds: GetItemsByIds) {
+    this.getItemsByIds = getItemsByIds
+  }
+
   public async handle ({ itemsIds }: GetItemsByIdsHttpInput): Promise<GetItemsByIdsHttpOutput> {
     if (!itemsIds) return badRequest(new MissingParamError('itemsIds'))
+
+    await this.getItemsByIds.execute({ ids: itemsIds.split(',') })
 
     return {
       items: []
