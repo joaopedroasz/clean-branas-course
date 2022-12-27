e033ba312fbfc953020723f24a4c154ed5e9c465
João Pedro <joao.pedroasz00@gmail.com>
2022-12-27 11:37:54 -0300

    test: ensure ItemRepository returns an array of items by ids

--- catalog/tests/integration/infra/database/repositories/ItemRepository.test.ts
+++ catalog/tests/integration/infra/database/repositories/ItemRepository.test.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
+import { Item } from '@/domain/entities'
 import { ItemRepository } from '@/domain/repositories/Item'
+import { ItemPrismaRepository } from '@/infra/database'
 import { connection, deleteAll } from '@/tests/utils'
 import { makeItem } from '@/tests/doubles'
-import { ItemPrismaRepository } from '@/infra/database'
 
 type SutType = {
   sut: ItemRepository
@@ -29,6 +30,37 @@ describe('ItemRepository', () => {
 
       const createdItem = await connection.item.findFirst()
       expect(createdItem).toBeTruthy()
+    })
+  })
+
+  describe('get items by ids', () => {
+    it('should return an array of items', async () => {
+      const { sut } = makeSut()
+      const item = makeItem()
+      await sut.save(item)
+      const createdItem = await connection.item.create({
+        data: {
+          description: item.getDescription(),
+          depth: item.getDepth(),
+          height: item.getHeight(),
+          price: item.getPrice(),
+          weight: item.getWeight(),
+          width: item.getWidth()
+        }
+      })
+
+      const items = await sut.getByIds([createdItem.id])
+
+      expect(items).toHaveLength(1)
+      expect(items[0]).toBeInstanceOf(Item)
+    })
+
+    it('should return an empty array if no items are found', async () => {
+      const { sut } = makeSut()
+
+      const items = await sut.getByIds(['invalid-id'])
+
+      expect(items).toHaveLength(0)
     })
   })
 })
